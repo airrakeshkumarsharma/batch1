5bf91f35ec2cde8a438619ce49c01dcd94800e2d
Rakesh <airrakeshkumarsharma@gmail.com>
2023-03-30 22:07:09 +0530

    Mini calulator has developed

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let x  = 5
+let y = 10
+let z = 20
+
+console.log("Add", x + y + z)
+console.log("sub", x - y + z)
+console.log("Mul", x * y + z)
+console.log("Div", x / y + z)
+console.log("Mod", x % y + z)
+
+// Day 1 -> two variables
+// Day 2 -> three variables
