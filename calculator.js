35008098f8626161e055546bfa2fa8f8e02cc123
Rakesh <airrakeshkumarsharma@gmail.com>
2023-04-03 22:01:32 +0530

    updated the cal file

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ let x  = 10
 let y = 10
 let z = 20
 
-console.log("Add", x + y + z)
+console.log("Add", x + y + z+3)
 console.log("sub", x - y + z)
 console.log("Mul", x * y + z)
 console.log("Div", x / y + z)
