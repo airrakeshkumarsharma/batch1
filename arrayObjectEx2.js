18b299b8d9e59b519d396d52dc6bec16081bc932
Rakesh <airrakeshkumarsharma@gmail.com>
2023-04-19 21:59:51 +0530

    added new object example

---
+++ arrayObjectEx2.js
@@ -0,0 +1,16 @@
+const bankAccountHolder = [
+    {
+        savingAccount: 2,
+        currentAccount: 1,
+        mobile: "32323232332",
+        name: "abc bcd",
+        email: "example@gmail.com"
+    },
+    {
+        savingAccount: 1,
+        currentAccount: 2,
+        mobile: "666666666",
+        name: "rohit",
+        email: "example2@gmail.com"
+    }
+]
